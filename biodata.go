5c97a4d5357602d4524c7b47a30f4ab49f180d10
basiliuskristiawan <basilius.kristiawan@koinworks.com>
2022-07-14 16:38:54 +0700

    struct golang

---
+++ biodata.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type student struct {
+	Nama      string
+	Alamat    string
+	Pekerjaan string
+	Alasan    string
+}
+
+type students []student
+
+func main() {
+	arg := os.Args[1]
+	stnds := students{
+		{
+			Nama:      "Lius",
+			Alamat:    "Bogor",
+			Pekerjaan: "Backend",
+			Alasan:    "Karena golang sedang populer",
+		},
+		{
+			Nama:      "Adrian",
+			Alamat:    "Bekasi",
+			Pekerjaan: "Backend",
+			Alasan:    "Karena golang sedang populer",
+		},
+		{
+			Nama:      "Abdul",
+			Alamat:    "Tangerang",
+			Pekerjaan: "Backend",
+			Alasan:    "Karena golang sedang populer",
+		},
+	}
+
+	stnds.Print(arg)
+
+}
+
+func (st students) Print(arg string) {
+	intVar, _ := strconv.Atoi(arg)
+	if intVar > len(st) {
+		fmt.Println("Data tidak ditemukan")
+		return
+	}
+
+	fmt.Println("Nama :", st[intVar-1].Nama)
+	fmt.Println("Alamat :", st[intVar-1].Alamat)
+	fmt.Println("Pekerjaan :", st[intVar-1].Pekerjaan)
+	fmt.Println("Alasan memilih kelas golang :", st[intVar-1].Alasan)
+}
